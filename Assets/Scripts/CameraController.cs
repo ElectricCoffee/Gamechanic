c49c8f380008b3fe681b52fa477595e52fe9c175
Radu Matusa <matusaradu@gmail.com>
2019-11-23 19:36:00 +0100

    Add various

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,18 +4,63 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
+    public Transform lookAt;
+    public float boundX = 100f;
+    public float boundZ = 100f;
+
+    public Collider collider;
+
+    private float maxX;
+    private float maxZ;
+
+    private Vector3 desiredPossition;
 
     // Use this for initialization
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        maxX = collider.transform.position.x;
+        maxZ = collider.transform.position.z;
+    }
+
+    void Update()
+    {
+        transform.position.x = Mathf.Clamp(transform.position.x, maxX, -maxX);
+        transform.position.z = Mathf.Clamp(transform.position.z, maxZ, -maxZ);
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        Vector3 delta = Vector3.zero;
+
+        float dx = lookAt.position.x - transform.position.x;
+        float dz = lookAt.position.z - transform.position.z;
+
+        if(dx > boundX || dx < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = dx - boundX;
+            }
+            else
+            {
+                delta.x = dx + boundX;
+            }
+        }
+
+        if (dz > boundZ|| dz < -boundZ)
+        {
+            if (transform.position.z < lookAt.position.z)
+            {
+                delta.z = dz - boundZ;
+            }
+            else
+            {
+                delta.z = dz + boundZ;
+            }
+        }
+
+        desiredPossition = transform.position + delta;
+        transform.position = Vector3.Lerp(transform.position, desiredPossition, 0.1f);
     }
 }
