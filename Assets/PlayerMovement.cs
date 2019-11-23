a572f15499cf6b491265bf8e0fd6fac89eaba40d
Niko Lepka <slench102+git@gmail.com>
2019-11-23 08:56:40 +0100

    more minor changes that I missed previously

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody myRigidbody;
+    private Rigidbody rb;
     private RaycastHit hit;
 
     public float speed;
@@ -22,16 +22,15 @@ public class PlayerMovement : MonoBehaviour
     private bool isJumping;
     private bool isDead;
 
-    private float moveInputVertical;
-    private float moveInputHorizontal;
+    private Vector2 moveInput;
     private Vector2 currentRotation;
-    private float health = 1f; // shouldn't this be a uint?
+    private uint health = 1;
     private float t; // what the hell is 't'?
 
     // Start is called before the first frame update
     void Start()
     {
-        myRigidbody = gameObject.GetComponent<Rigidbody>();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -40,29 +39,30 @@ public class PlayerMovement : MonoBehaviour
         Time.timeScale = timeSpeed; // Set time speed
 
         // Kill player if health is 0 or mechanic is off
-        if (Math.Abs(health) < Mathf.Epsilon || mechanicHealth == false)
+        if (health == 0 || !mechanicHealth)
         {
             Kill();
         }
 
-        if (mechanicJump == true)
+        if (mechanicJump)
         {
             var keyPressed = Input.GetKeyDown(KeyCode.Space);
             // Jumping when there is a hole in front
-            if (canJump == true && keyPressed && isJumping == false)
+            if (canJump && keyPressed && !isJumping)
             {
                 t = 0;
                 isJumping = true;
-                myRigidbody.velocity = new Vector3 (
+                rb.velocity = new Vector3 (
                     currentRotation.x * jumpForce,
                     jumpForce / 2,
                     currentRotation.y * jumpForce);
             }
         }
 
-        isJumping &= (canJump != false || t < 0.5f);
+        isJumping &= (canJump || t < 0.5f);
 
-        if (isJumping == true) // Timer for jumping only for 0.5 seconds
+        // Timer for jumping only for 0.5 seconds
+        if (isJumping)
         {
             if (t < 0.5f)
             {
@@ -75,38 +75,39 @@ public class PlayerMovement : MonoBehaviour
     {
         // Raycast that checks if there is a hole in front of the Player
         canJump = !Physics.Raycast(
-            myRigidbody.position + (transform.forward * 4),
+            rb.position + (transform.forward * 4),
             Vector3.down,
             out hit,
             13.5f);
 
 
-        myRigidbody.velocity += Physics.gravity * gravityModifier * Time.fixedDeltaTime;
+        rb.velocity += Physics.gravity * gravityModifier * Time.fixedDeltaTime;
 
-        moveInputHorizontal = Input.GetAxisRaw("Horizontal");
-        moveInputVertical = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"));
 
         // Method for setting facing direction
-        Rotate(new Vector2(moveInputHorizontal, moveInputVertical));
+        Rotate(moveInput);
 
         // Vector showing where is the Player facing
         Vector3 movement = new Vector3(currentRotation.x, 0.0f, currentRotation.y);
 
         // Velocity before new velocity is created
-        Vector3 past = new Vector3(myRigidbody.velocity.x, 0, myRigidbody.velocity.z);
+        Vector3 past = new Vector3(rb.velocity.x, 0, rb.velocity.z);
 
         if (mechanicMovement)
         {
             if (isJumping && t >= 0.5f)
             {
-                myRigidbody.velocity = new Vector3(0, myRigidbody.velocity.y, 0) + past;
+                rb.velocity = new Vector3(0, rb.velocity.y, 0) + past;
             }
-            else if (!isJumping && (t <= 0.55f || Math.Abs(t) < 0.001f))
+            else if (!isJumping && (t <= 0.55f || Math.Abs(t) < Mathf.Epsilon))
             {
-                myRigidbody.velocity = new Vector3(
-                    moveInputHorizontal * speed,
-                    myRigidbody.velocity.y,
-                    moveInputVertical * speed);
+                rb.velocity = new Vector3(
+                    moveInput.x * speed,
+                    rb.velocity.y,
+                    moveInput.y * speed);
             }
         }
 
