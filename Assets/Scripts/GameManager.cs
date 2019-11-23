3ba601495066640ac5b243a6136ae0ea514cc990
Steven Teglman <steventeglman@gmail.com>
2019-11-23 21:56:09 +0100

    create desk and monitor

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMesh textMesh;
+    public int startTime = 8;
+    private string time;
+    public GameObject textMeshOB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = textMeshOB.GetComponent<TextMesh>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setTime(int hour)
+    {
+        string newTime = $"0{hour}:00";
+        textMesh.text = newTime;
+    }
+}
