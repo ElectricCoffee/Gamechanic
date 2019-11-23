81c19acfd72668a252f0a3253a3755859bb7e589
MohanadZ <mzeito14@student.aau.dk>
2019-11-23 09:28:05 +0100

    Added image sprite

--- Assets/Scripts/DebugMode.cs
+++ Assets/Scripts/DebugMode.cs
@@ -6,16 +6,31 @@ using TMPro;
 
 public class DebugMode : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI kBAvailable, kBUsed;
     [SerializeField] Button movementOption;
+    [SerializeField] Image[] xMarkImages;
+    [SerializeField] Sprite imageSprite;
     
     void Start()
     {
         movementOption.GetComponent<Button>();
+        foreach(Image img in xMarkImages)
+        {
+            img.GetComponent<Image>();
+            Color newColor = img.color;
+            newColor.a = 0f;
+            img.color = newColor;
+        }
     }
     
     public void MovementOptionPress()
     {
         Debug.Log(movementOption.gameObject.name + " was pressed.");
+        ChangeImage(0);
+    }
+
+    public void ChangeImage(int imageIndex)
+    {
+        xMarkImages[imageIndex].sprite = imageSprite;
+        xMarkImages[imageIndex].color = new Color(1, 1, 1, 1);
     }
 }
