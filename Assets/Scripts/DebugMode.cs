9affbe1836ed2bcc404d6144c3e6c48e300a2531
MohanadZ <mzeito14@student.aau.dk>
2019-11-23 07:12:12 +0100

    Added the canvas to a separate scene

---
+++ Assets/Scripts/DebugMode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DebugMode : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI kBAvailable, kBUsed;
+    [SerializeField] Button movementOption;
+    
+    void Start()
+    {
+        movementOption.GetComponent<Button>();
+    }
+    
+    public void MovementOptionPress()
+    {
+        Debug.Log(movementOption.gameObject.name + " was pressed.");
+    }
+}
