311a3a07fb5cf517eddd84ce3b39ec6ca756ad16
Radu Matusa <matusaradu@gmail.com>
2019-11-23 03:36:12 +0100

    Jumping done, Added comments

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+    private Vector3 offset;
 
     // Use this for initialization
     void Start()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = player.transform.position + offset;
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,13 +10,16 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed;
     public float gravityModifier;
+    public float jumpForce;
 
-    public bool canJump;
+    private bool canJump;
+    private bool isJumping;
 
     private float moveInputVertical;
     private float moveInputHorizontal;
     private float horizontalRotation;
     private float verticalRotation;
+    private float t;
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +30,29 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (canJump == true && Input.GetKeyDown(KeyCode.Space)) // Jumping when there is a hole in front
+        {
+            t = 0;
+            isJumping = true;
+            myRigidbody.velocity = new Vector3(horizontalRotation * jumpForce, jumpForce / 2, verticalRotation * jumpForce);
+        }
+        if (canJump == true && t >= 0.1f) // Stoping jump
+        {
+            isJumping = false;
+        }
+
+        if (isJumping == true) // Timer for jumping only for 0.1 seconds
+        {
+            if (t < 0.1f)
+            {
+                t += Time.deltaTime;
+            }
+        }
     }
 
     void FixedUpdate()
     {
-        Debug.DrawRay(new Vector3(myRigidbody.position.x, myRigidbody.position.y, myRigidbody.position.z) + (transform.forward * 4), Vector3.down * 45f, Color.green);
-        canJump = Physics.Raycast(new Vector3(myRigidbody.position.x, myRigidbody.position.y, myRigidbody.position.z) + (transform.forward * 4), Vector3.down, out hit , 45f);
+        canJump = !Physics.Raycast(new Vector3(myRigidbody.position.x, myRigidbody.position.y, myRigidbody.position.z) + (transform.forward * 4), Vector3.down, out hit , 13.5f); // Raycast that checks if there is a hole in front of the Player
         
 
         myRigidbody.velocity += Physics.gravity * gravityModifier * Time.deltaTime;
@@ -40,16 +60,22 @@ public class PlayerMovement : MonoBehaviour
         moveInputHorizontal = Input.GetAxisRaw("Horizontal");
         moveInputVertical = Input.GetAxisRaw("Vertical");
 
-        Rotate(new Vector2(moveInputHorizontal, moveInputVertical));
+        Rotate(new Vector2(moveInputHorizontal, moveInputVertical)); // Method for setting facing direction
 
-        Vector3 movement = new Vector3(horizontalRotation, 0.0f, verticalRotation);
+        Vector3 movement = new Vector3(horizontalRotation, 0.0f, verticalRotation); // Vector showing where is the Player facing
 
-        print(movement);
+        Vector3 past = new Vector3(myRigidbody.velocity.x, 0, myRigidbody.velocity.z); // Velocity before new velocity is created
 
-        myRigidbody.velocity = new Vector3(moveInputHorizontal * speed, myRigidbody.velocity.y, moveInputVertical * speed);
+        if(isJumping == true && t >= 0.1f)
+        {
+            myRigidbody.velocity = new Vector3(moveInputHorizontal * speed, myRigidbody.velocity.y, moveInputVertical * speed) + past; // Movement if it jumps
+        }
+        else if(isJumping == false && (t <= 0.15f || Math.Abs(t) < 0.001f))
+        {
+            myRigidbody.velocity = new Vector3(moveInputHorizontal * speed, myRigidbody.velocity.y, moveInputVertical * speed); // Movement if it doesn't jump
+        }
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
-
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F); // Rotating Player
     }
 
     private void Rotate(Vector2 vector)
